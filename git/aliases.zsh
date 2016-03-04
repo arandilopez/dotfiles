2e752c5bd2c670d2b0a04d33bd6aa67b9a98c1ba
Arandi Lopez <arandilopez.93@gmail.com>
2016-03-04 14:48:29 -0600

    git alias and atom changes

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -22,3 +22,5 @@ alias gb='git branch'
 alias gs='git status' # upgrade your git if -sb breaks for you. it's fun.
 alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
+alias gw='git whatchanged'
+alias gsw='git show'
