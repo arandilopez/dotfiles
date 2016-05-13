18f3f77927449bf10e90fb1c18ec4aa6cd1c73f2
Arandi Lopez <arandilopez.93@gmail.com>
2016-05-13 14:39:54 -0500

    alias para firmar las apk de ionic

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+
+if (( $+commands[ionic] ))
+then
+  alias apk-sign='ionic build --release android -- --keystore=~/Code/arandilopez.keystore --storePassword=arandilopez --alias=arandilopez --password=arandilopez';
+fi
