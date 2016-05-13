c2630ce0c869d9b30b1229f80c2003c38d3cda98
Arandi Lopez <arandilopez.93@gmail.com>
2016-05-13 13:08:04 -0500

    configs lol

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,4 @@
 export ANDROID_HOME="$HOME/Library/Android/sdk";
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$HOME/.composer/vendor/bin:/opt/boxen/homebrew/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
