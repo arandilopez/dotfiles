55c2b2391be331bae9e1273f7c0855a958c77a9b
Arandi López <arandilopez.93@gmail.com>
2017-03-05 06:44:50 -0600

    rails and bundler aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,10 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+alias r='rails $*'
+alias rs='rails server $*'
+
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
