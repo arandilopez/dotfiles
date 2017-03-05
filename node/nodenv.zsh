bdc1c6ae0095b3e8b5e07d3e7f9b08d5b2f32f96
Arandi López <arandilopez.93@gmail.com>
2017-03-05 05:49:06 -0600

    meh

--- node/nodenv.zsh
+++
@@ -1,5 +0,0 @@
-# init according to man page
-if (( $+commands[nodenv] ))
-then
-  eval "$(nodenv init -)"
-fi
--- system/_path.zsh
+++ system/_path.zsh
@@ -2,3 +2,6 @@ export ANDROID_HOME="$HOME/Library/Android/sdk";
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$HOME/.anaconda2/bin:$HOME/.composer/vendor/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:/usr/local/texlive/2016/bin/x86_64-darwin:$PATH"
 export JAVA_HOME=$(/usr/libexec/java_home)
+
+export SOCKET_DIR="/usr/local/var/www"
+export LOG_DIR="/usr/local/var/log/nginx"
