512dcbbdb5273106e10f29ff1d49bf62448059ff
Arandi López <arandilopez.93@gmail.com>
2017-05-11 11:43:42 -0500

    mmm...

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,10 +1,10 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git='hub'
-fi
+# hub_path=$(which hub)
+# if (( $+commands[hub] ))
+# then
+alias git='hub'
+# fi
 
 # The rest of my fun git aliases
 alias g='hub'
