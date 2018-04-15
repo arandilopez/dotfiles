dd165487133275a613f65db3de2bebbead292823
Arandi Lopez <arandilopez.93@gmail.com>
2018-04-14 19:24:12 -0500

    Remove JAVA_HOME declaration here

--- android/path.zsh
+++ android/path.zsh
@@ -1,3 +1,3 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
+# export JAVA_HOME=$(/usr/libexec/java_home)
 export ANDROID_HOME="$HOME/Library/Android/sdk";
 export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$PATH"
