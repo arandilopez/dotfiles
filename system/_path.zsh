0dd6466c2eb84e6b31ff1bbdbe2074d7f2354e15
Arandi Lopez <arandilopez.93@gmail.com>
2018-04-14 19:38:47 -0500

    Add fixes for rbenv and nodenv on linux

--- system/_path.zsh
+++ system/_path.zsh
@@ -3,3 +3,11 @@ export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:/usr/local
 
 export SOCKET_DIR="/usr/local/var/www"
 export LOG_DIR="/usr/local/var/log/nginx"
+
+if [ "$(uname -s)" == "Linux" ]; then
+  export PATH="$HOME/.nodenv/bin:$PATH"
+  export PATH="$HOME/.nodenv/plugins/node-build/bin:$PATH"
+
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+fi
