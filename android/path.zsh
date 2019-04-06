9b94dfbbeaa54a645e69f623b0fa1b0c98a463f0
Arandi Lopez <arandilopez.93@gmail.com>
2019-04-06 12:16:44 -0600

    Export android binaries

--- android/path.zsh
+++ android/path.zsh
@@ -8,4 +8,4 @@ case `uname` in
     export ANDROID_HOME="$HOME/Library/Android/sdk";
     ;;
 esac
-export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools:$PATH"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/build-tools/28.0.3:$PATH"
