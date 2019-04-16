bbace321d88d484bcca2350480cd47d385c51dd9
Arandi Lopez <arandilopez.93@gmail.com>
2019-04-16 18:51:04 -0500

    Aliases for postgresql in osx

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,5 @@
 alias reload!='source ~/.zshrc'
+if [[ `uname` == 'Darwin' ]]; then
+  alias pgstart='pg_ctl -D /usr/local/var/postgres start'
+  alias pgstop='pg_ctl -D /usr/local/var/postgres stop'
+fi
