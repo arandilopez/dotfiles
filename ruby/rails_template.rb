69b1fc3b2cfa5b174fca92c9132152e800caccd3
Arandi Lopez <arandilopez.93@gmail.com>
2019-04-16 18:47:48 -0500

    update rails project template

--- ruby/rails_template.rb
+++ ruby/rails_template.rb
@@ -10,8 +10,14 @@ gsub_file 'Gemfile', /(^#|^. #).*?\n+/, ''
 gsub_file 'Gemfile', /^[\r\n]+/, "\n"
 enable_webpack = yes?("Enable webpack?")
 enable_bcrypt = yes?("Enable bcrypt for has_secure_password?")
+enable_devise = yes?("Enable devise for authenticacion?") unless enable_bcrypt
+enable_delayed_job = yes?("Enable delayed job for jobs?")
+enable_haml = yes?("Enable haml?")
+gem "haml-rails", "~> 1.0" if enable_haml
 gem 'bcrypt' if enable_bcrypt
+gem 'devise' if enable_devise
 gem 'webpacker', '~> 3.4' if enable_webpack
+gem 'delayed_job_active_record' if enable_delayed_job
 gem 'faker'
 gem_group :development, :test do
   gem 'dotenv-rails'
@@ -20,11 +26,36 @@ end
 
 run 'bundle install'
 
+if enable_haml
+  ENV['HAML_RAILS_DELETE_ERB'] = 'true'
+  rails_command 'haml:erb2haml'
+end
+
+if enable_webpack
+  rails_command 'webpacker:install'
+  rails_command 'webpacker:install:vue'
+end
+
+if enable_delayed_job
+  generate 'delayed_job:active_record'
+end
+
+if enable_devise
+  generate 'devise:install'
+  generate 'devise user'
+end
+
 ##############################
 ## CREATE FILE FOR ENV VARIABLES
 ##############################
-create_file '.env'
-create_file '.env.example'
+%w{.env .env.example}.each do |env_file|
+  file env_file, <<-TEXT
+  DATABASE_USER=
+  DATABASE_PASSWORD=
+  DATABASE_PORT=5432
+  DATABASE_HOST=localhost
+  TEXT
+end
 append_to_file '.gitignore', ".env\n"
 
 ##############################
@@ -38,17 +69,13 @@ end
 ##############################
 ## MODEL & DB
 ##############################
-if yes?('Generate a user-model?')
-  params = [:model, 'user', 'name', 'email']
-  params << 'password_digest' if enable_bcrypt
-  generate(*params)
-  run 'rails db:create db:migrate'
-  gsub_file 'app/models/user.rb', "Record\n", "Record\n\thas_secure_password\n" if enable_bcrypt
-end
-
-if enable_webpack
-  rails_command 'webpacker:install'
-  rails_command 'webpacker:install:vue'
+unless File.exist?('app/models/user.rb')
+  if yes?('Generate a user-model?')
+    params = [:model, 'user', 'email']
+    params << 'password_digest' if enable_bcrypt
+    generate(*params)
+    gsub_file 'app/models/user.rb', "Record\n", "Record\n\thas_secure_password\n" if enable_bcrypt
+  end
 end
 
 environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
@@ -59,4 +86,7 @@ after_bundle do
   run 'spring stop'
   git add: '.'
   git commit: "-m 'Initial commit'"
+  puts "****************************************"
+  puts "Don't forget to migrate your db!"
+  puts "****************************************"
 end
