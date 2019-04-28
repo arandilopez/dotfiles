26cd3258c6a6230c3debae3a01b9f74af65ab1e8
Arandi Lopez <arandilopez.93@gmail.com>
2019-04-28 12:59:12 -0500

    rails dbconsole alias

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,9 +1,9 @@
-
 alias r='rails'
 alias rt='rails test'
 alias rts='rails test test/system'
 alias rs='rails server'
 alias rc='rails console'
+alias rdb='rails dbconsole'
 
 alias be='bundle exec'
 alias bi='bundle install'
