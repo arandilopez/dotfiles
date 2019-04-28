234249199f4a4f3df64950258eda2121b3a5b048
Arandi Lopez <arandilopez.93@gmail.com>
2019-04-28 12:59:51 -0500

    Vim as default editor

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,2 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
-
-export SOCKET_DIR="/usr/local/var/www"
-export LOG_DIR="/usr/local/var/log/nginx"
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
