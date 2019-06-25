79f5d92494664f855c4fbfee45eab54c0e927160
Arandi Lopez <arandilopez.93@gmail.com>
2019-06-25 18:10:53 -0500

    Change some git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,13 +5,15 @@ alias git='hub'
 
 # The rest of my fun git aliases
 alias g='hub'
-alias gl='git pull'
-alias glr='git pull --rebase --autostash'
+# alias gl='git pull'
+alias gl='git pull --rebase --autostash'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gpt='git push origin HEAD && git track'
 alias gd='git diff'
-alias gc='git commit -m'
+alias gac='git add -A && git commit -m'
+alias gc='git commit'
+alias gcm='git commit -m'
 alias gcam='git commit -am'
 alias gco='git checkout'
 alias gcob='git checkout -b'
@@ -19,7 +21,6 @@ alias gbn='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status'
 alias gst='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
-alias gac='git add -A && git commit -m'
 alias gw='git whatchanged'
 alias gsw='git show'
 alias ga='git add'
@@ -28,6 +29,7 @@ alias gsh='git stash'
 alias gshp='git stash pop'
 alias grm="git rebase-master"
 alias gsrm="git stash-rebase-master"
-alias gml="git co master && git pull"
+alias gml="git co master && git pull --rebase --autostash"
 alias gdlm="git delete-local-merged"
 alias nah="git clean -df && git checkout -- ."
+alias wtf="git wtf"
