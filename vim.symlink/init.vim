7cdcdd90058081d846cea6fb30e9f918b020abce
Arandi Lopez <arandilopez.93@gmail.com>
2020-05-02 13:32:55 -0500

    Link neovim to vim

---
+++ vim.symlink/init.vim
@@ -0,0 +1,1 @@
+/home/arandilopez/.vimrc
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,6 @@ if [[ $OS_NAME == 'Darwin' ]]; then
   alias pgstart='pg_ctl -D /usr/local/var/postgres start'
   alias pgstop='pg_ctl -D /usr/local/var/postgres stop'
 fi
+if type nvim > /dev/null 2>&1; then
+  alias vim='nvim'
+fi
