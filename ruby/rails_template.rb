6081f231f821fa2490f87e191852b04f2ce391d4
Arandi Lopez <arandilopez.93@gmail.com>
2020-05-17 19:31:44 -0500

    Fix rails template and add support for bootstrap

--- ruby/rails_template.rb
+++ ruby/rails_template.rb
@@ -1,10 +1,38 @@
-APP_NAME = @app_name.titleize
-RAILS_VERSION = File.read('Gemfile').scan(%r{(?<=gem 'rails', '~> ).*(?=')}).first.to_f
+def yarn(*packages)
+  run("yarn add #{packages.join(" ")}")
+end
 
 ##################
 # Methods
 ##################
-def install_webpack?
+
+def install_bootstrap?
+  yarn 'jquery', 'popper.js', 'bootstrap'
+
+  inject_into_file 'config/webpack/environment.js', after: "const { environment } = require('@rails/webpacker')\n" do <<~EOF
+    const webpack = require('webpack')
+    environment.plugins.append('Provide', new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    }))
+  EOF
+  end
+
+  inject_into_file 'app/views/layouts/application.html.erb', before: '</head>' do <<~EOF
+    <%= stylesheet_pack_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+  EOF
+  end
+
+  inject_into_file 'app/javascript/packs/application.js' do <<~EOF
+  import 'bootstrap/dist/js/bootstrap'
+  import 'bootstrap/dist/css/bootstrap'
+  EOF
+  end
+end
+
+def install_frontend_framework?
   driver = ask("Which front-end framework will you use? [vue, react, angular, stimulus] (default: vue)")
   driver = 'vue' if driver.blank?
   rails_command "webpacker:install:#{driver}"
@@ -12,22 +40,28 @@ end
 
 def install_devise?
   return unless yes?("Would you like to install Devise?")
-  gem "devise"
+  run "bundle add devise"
+  run 'bundle install'
   generate "devise:install"
   environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
   environment "config.action_mailer.delivery_method = :smtp", env: 'development'
   environment "config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }", env: 'development'
   model_name = ask("What would you like the user model to be called? [user]")
   model_name = "user" if model_name.blank?
-  generate "devise", model_name
+  attributes = ""
+  if yes?("Do you want to any extra attributes to #{model_name}? [y/n]")
+    attributes = ask("What attributes?")
+  end
+  run "rails generate devise #{model_name} #{attributes}"
 end
 
 def install_delayed_job?
   return unless yes? "Would you like to install Delayed Job?"
-  gem 'delayed_job_active_record'
+  run 'bundle add delayed_job_active_record'
+  run 'bundle install'
   generate "delayed_job:active_record"
   environment 'config.active_job.queue_adapter = :delayed_job', env: :production
-  initializer "active_job.rb", <<~TEXT
+  initializer "delayed_job_queues.rb", <<~TEXT
   Delayed::Worker.queue_attributes = {
     high_priority: { priority: 0 },
     default: { priority: 5 },
@@ -39,7 +73,8 @@ end
 
 def install_sidekiq?
   return unless yes?("Would you like to install sidekiq?")
-  gem 'sidekiq'
+  run 'bundle add sidekiq'
+  run 'bundle install'
   environment 'config.active_job.queue_adapter = :sidekiq', env: :production
   environment "config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'", env: :production
   prepend_to_file 'config/routes.rb', "require 'sidekiq/web'\n\n"
@@ -53,7 +88,8 @@ end
 
 def install_postmarkapp?
   return unless yes?("Would you like to instal postmarkapp for mailing?")
-  gem 'postmark-rails'
+  run 'bundle add postmark-rails'
+  run 'bundle install'
   environment 'config.action_mailer.delivery_method = :postmark', env: :production
   environment 'config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }', env: :production
   puts 'Save your Postmark Server API Token to `config/credentials.yml.enc` by running `rails secret then rails credentials:edit`'
@@ -61,8 +97,9 @@ end
 
 def install_haml?
   return unless yes? "Would you like to install Haml and convert your erb views to haml?"
-  gem 'hamlit-rails'
-  # rails_command 'hamlit:erb2haml'
+  run 'bundle add hamlit-rails'
+  run 'bundle install'
+  rails_command 'hamlit:erb2haml'
 end
 
 # Everything starts here!
@@ -95,8 +132,8 @@ remove_file('config/database.yml')
 file 'config/database.yml', <<~YAML
 default: &default
   adapter: postgresql
-  username: ENV.fetch('DATABASE_USERNAME') { 'postgres' }
-  password: ENV.fetch('DATABASE_PASSWORD') { 'postgres' }
+  username: <%= ENV.fetch('DATABASE_USERNAME') { 'postgres' } %>
+  password: <%= ENV.fetch('DATABASE_PASSWORD') { 'postgres' } %>
   encoding: unicode
   pool: <%= ENV.fetch("RAILS_MAX_THREADS") { 5 } %>
 
@@ -116,22 +153,33 @@ file 'Procfile', <<~TEXT
   web: bundle exec rails server -p $PORT
 TEXT
 
+# Setup factory_bot_rails
+File.open(Rails.root.join("test", "test_helper.rb"), "r+") do |file|
+  lines = file.each_line.to_a
+  config_index = lines.map.with_index { |line, index| index if line == "end\n" }.last
+  lines.insert(config_index, "  include FactoryBot::Syntax::Methods\n");
+  file.rewind
+  file.write(lines.join)
+end
+
 after_bundle do
-  install_webpack?
+  run 'spring stop'
+
+  install_frontend_framework?
+  install_bootstrap?
   install_devise?
   install_delayed_job?
   install_sidekiq?
-  install_haml?
   install_postmarkapp?
+  install_haml?
+
   if yes?('Generate home controller and root path?')
     generate :controller, 'home index'
     gsub_file 'config/routes.rb', "get 'home/index'", "root to: 'home#index'"
   end
 
-
   application "config.time_zone = 'America/Mexico_City'"
 
-  run 'spring stop'
   git add: '.'
   git commit: "-m 'Initial commit'"
   puts "****************************************"
