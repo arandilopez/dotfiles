8ba43828c31f458be3a7fcdefd2aafc063b74d48
Arandi Lopez <arandilopez.93@gmail.com>
2021-06-27 14:19:39 -0500

    add new location to path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,2 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$ZSH/bin:$HOME/.bin:$HOME/.local/bin:$PATH"
