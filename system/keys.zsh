7569b34e3a55a2e0eeba89882643381310b384a4
Arandi Lopez <arandilopez.93@gmail.com>
2024-02-28 12:14:33 -0600

    feat: update pubkey function

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,9 +1,9 @@
 # Pipe my public key to my clipboard.
 case $OS_NAME in
   Darwin )
-    alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+    alias pubkey="more ~/.ssh/id_ed25519.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
     ;;
   Linux )
-    alias pubkey="xclip -sel clip < ~/.ssh/id_rsa.pub && echo '=> Public key copied to pasteboard.'"
+    alias pubkey="xclip -sel clip < ~/.ssh/id_ed25519.pub && echo '=> Public key copied to pasteboard.'"
     ;;
 esac
