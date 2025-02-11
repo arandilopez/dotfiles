e4d9326b3cb4c94d95fa764053edbbafb3678f62
Arandi Lopez <arandilopez.93@gmail.com>
2025-02-11 11:41:53 -0600

    feat: missing commit for changes on workflow with rails and git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,9 +1,9 @@
 # Use `hub` as our git wrapper:
 #   http://defunkt.github.com/hub/
 # Always use hub
-alias git='hub'
+# alias git='hub'
 # The rest of my fun git aliases
-alias g='hub'
+alias g='git'
 alias gl='git pull --rebase --autostash'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset [%an] %s - %Creset%C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
